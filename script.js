c10a6e008ce695dfe0102ed1079dc034e8414989
Alex Campbell <campbellar2@vcu.edu>
2022-04-04 14:46:44 -0400

    Created grid layout, buttons, and hover effect

--- script.js
+++ script.js
@@ -1,1 +1,30 @@
-const container = document.querySelector("#container")
+// important variables from html
+const container = document.querySelector("#grid");
+let rows = document.getElementsByClassName("row");
+let cells = document.getElementsByClassName("cell");
+
+//creates the grid
+function makeGrid(num) {
+    for (i = 0; i < num; i++) {
+        let row = document.createElement("div");
+        container.appendChild(row).className = "row";
+        for (j=0; j < num; j++) {
+            let cell = document.createElement("div");
+            row.appendChild(cell).className = "cell";
+        }
+    };
+}
+
+//creates default grid
+makeGrid(16)
+
+//hover effect on grid
+//cells.addEventListener('mouseenter', (event) => {
+ //   event.target.classList.add("hovered");
+//})
+
+for (i = 0; i < cells.length; i ++) {
+    cells[i].addEventListener('mouseenter', (event) => {
+        event.target.classList.add("hovered");
+    })
+}
