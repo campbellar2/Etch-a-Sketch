44c21f795dd621205c26225fa4e4a91617442b00
Alex Campbell <campbellar2@vcu.edu>
2022-04-06 00:02:52 -0400

    added functionality to size btn

--- script.js
+++ script.js
@@ -1,13 +1,14 @@
 // important variables from html
 const container = document.querySelector("#grid");
 const btnClear = document.getElementById("clear");
-const btnSize = document.querySelector("#size");
+const btnSize = document.getElementById("size");
 
 let rows = document.getElementsByClassName("row");
 let cells = document.getElementsByClassName("cell");
 
 // assigning functions to buttons
 btnClear.onclick = clearBoard;
+btnSize.onclick = resizeBoard;
 
 //creates the grid
 function makeGrid(num) {
@@ -27,15 +28,24 @@ function makeGrid(num) {
     }
 }
 
-//creates default grid
-makeGrid(24)
-
 //clear board function
 function clearBoard() {
     for (i = 0; i < cells.length; i ++) {
         cells[i].classList.remove("hovered");
     };
 }
+
+//function to change the size of the board based on user input
+function resizeBoard() {
+    let num = parseInt(prompt("Please input the size of the new board (1-100). \n e.g. 24 = a 24x24 size board", "24"));
+    if (num > 200 || num < 0 || num === NaN) {
+        alert("Error, please select a number from 1 to 100");
+        return;
+    };
+    deleteBoard();
+    makeGrid(num);  
+}
+
 
 //deletes the old board
 function deleteBoard() {
@@ -44,3 +54,5 @@ function deleteBoard() {
     };
 }
 
+//creates default grid
+makeGrid(24)
